86c82a5ade6e2639a0427a85a8f4c6e837d25fd5
Fernando Santos Pereira <92190031+Fernandosantos0@users.noreply.github.com>
2022-02-02 21:34:22 -0300

    v2

--- script.php
+++ script.php
@@ -1,3 +1,21 @@
 <?php
 
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Formulário para preenchimento sobre dev">
+        <meta name="keywords" content="tecnologia, formulário, preencher">
+        <meta name="author" content="Fernando Santos Pereira">
+        <link rel="author" href="#">
+        <title>Formulário</title>
+        <link rel="shortcut icon" href="image/icon_paginas.ico" type="image/ico">
+    </head>
+    <body>
+
+    </body>
+</html>
